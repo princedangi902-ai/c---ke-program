7b5d7db02c665165e989c2d4a1b01ddcdd541b69
princedangi902-ai <princedangi902@gmail.com>
2026-07-16 23:21:58 +0530

    logic error fixed for primenumber.cpp

--- loops/primenumber.cpp
+++ loops/primenumber.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
-int main() {
+int main()
+{
     int n;
-    cout<<"enter a number: ";
-    cin>>n;
-    for(int i=2;i<=n/2;i++){
-        if(n%i==0){
-            cout<<"not a prime number!!";
-            break;
+    cout << "enter a number: ";
+    cin >> n;
+    bool isPrime = true;
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            isPrime = false;
         }
-        else{
-            cout<<"prime number";
+        else
+        {
+            cout << "prime number";
             break;
         }
     }
