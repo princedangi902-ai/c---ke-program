c0747cc1f87af3c77954c1537d9979a104d40154
princedangi902-ai <princedangi902@gmail.com>
2026-07-17 16:50:58 +0530

    conditonals and loops folder updated

--- loops/patternprintingstartriangle.cpp
+++ loops/patternprintingstartriangle.cpp
@@ -8,9 +8,9 @@ int main()
 
     for (int i = 1; i <= a; i++)
     {
-        for (int j =1; j <= i; j++)
+        for (int j =1; j <= i; j++){
             cout << "*";
-
+        }
         cout << endl;
     }
 
